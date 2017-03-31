249644a276c2b9a83d4a91db66a008c7196f4919
Fernando Nassif <fernandonassif@fea.local>
2017-03-30 22:00:33 -0300

    Fizemos as classes pro livro

--- src/Livro.java
+++
@@ -1,5 +0,0 @@
-/**
- * Forged by Soter Padua on 30/03/17.
- */
-public class Livro {
-}
---
+++ src/LivroBean.java
@@ -0,0 +1,44 @@
+/**
+ * Forged by Soter Padua on 30/03/17.
+ */
+public class LivroBean {
+	
+	private String titulo;
+	private Autor autor;
+	private Assunto assunto;
+	private Editora editora;
+	private Double valor;
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public Autor getAutor() {
+		return autor;
+	}
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	public Assunto getAssunto() {
+		return assunto;
+	}
+	public void setAssunto(Assunto assunto) {
+		this.assunto = assunto;
+	}
+	public Editora getEditora() {
+		return editora;
+	}
+	public void setEditora(Editora editora) {
+		this.editora = editora;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
