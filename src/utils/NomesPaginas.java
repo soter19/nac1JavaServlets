bf37a1289daff9f4d539f8ffa4d5d668c3f5805e
Soter Padua <spadua@2mundos.net>
2017-03-30 22:11:45 -0300

    Adicionado ENUM para os nomes das páginas.

---
+++ src/utils/NomesPaginas.java
@@ -0,0 +1,15 @@
+package utils;
+
+/**
+ * ENUM responsável por guardar todos os nomes de páginas, para facilitar
+ * quando o retorno dos beans.
+ */
+public enum NomesPaginas {
+	CARRINHO("carrinho"),
+	PESQUISA("pesquisa");
+
+	final public String nome;
+	NomesPaginas(final String nome) {
+		this.nome = nome;
+	}
+}
