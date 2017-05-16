29af74cf4a15b13e0ed058c2a93fa7d25e929e2c
Soter Padua <spadua@2mundos.net>
2017-05-16 18:16:33 -0300

    Construtor e fromDBObj implementado.

--- src/livraria/beans/Assunto.java
+++ src/livraria/beans/Assunto.java
@@ -1,10 +1,44 @@
 package livraria.beans;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCollection;
+import com.mongodb.client.MongoCollection;
+import livraria.bd.Collections;
+import livraria.bd.LivrariaBD;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 /**
  * Forged by Soter Padua on 03/04/17.
  */
 public class Assunto {
+	private String id;
 	private String titulo;
+
+	public Assunto(String id) throws Exception {
+		BasicDBObject query = new BasicDBObject("_id", new ObjectId(id));
+		Document      first = getCollection().find(query).first();// Weird but https://jira.mongodb.org/browse/JAVA-1744
+
+		if(first == null){
+			throw new Exception("Assunto not found");
+		}
+
+		this.titulo = first.getString("titulo");
+	}
+
+	private MongoCollection<Document> getCollection() {
+		return LivrariaBD.getInstancia()
+		                 .getBD()
+		                 .getCollection(Collections.ASSUNTOS.nome);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getTitulo() {
 		return titulo;
--- src/livraria/beans/Autor.java
+++ src/livraria/beans/Autor.java
@@ -1,10 +1,37 @@
 package livraria.beans;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.client.MongoCollection;
+import livraria.bd.Collections;
+import livraria.bd.LivrariaBD;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 /**
  * Forged by Soter Padua on 03/04/17.
  */
 public class Autor {
+	private String id;
 	private String nome;
+
+	public Autor(String id) throws Exception {
+		BasicDBObject query = new BasicDBObject("_id", new ObjectId(id));
+		Document      first = getCollection().find(query).first();// Weird but https://jira.mongodb.org/browse/JAVA-1744
+
+		if(first == null){
+			throw new Exception("Autor not found");
+		}
+
+		this.nome = first.getString("nome");
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	public String getNome() {
 		return nome;
@@ -12,5 +39,11 @@ public class Autor {
 
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+
+	private static MongoCollection<Document> getCollection(){
+		return LivrariaBD.getInstancia()
+		                 .getBD()
+		                 .getCollection(Collections.AUTORES.nome);
 	}
 }
