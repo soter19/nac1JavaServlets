046782e1f84d472e7ebba5efba8db743060a9096
Soter Padua <spadua@2mundos.net>
2017-05-19 13:26:55 -0300

    Banco de dados único agora ;)

--- src/livraria/Config.java
+++ src/livraria/Config.java
@@ -6,7 +6,7 @@ package livraria;
 public class Config {
 
 	//	BANCO DE DADOS
-	private final static String MONGO_IP       = "127.0.0.1";
+	private final static String MONGO_IP       = "soter.ninja";
 	private final static String MONGO_PORT     = "27017"; // porta padrão
 	public  final static String MONGO_HOST     = MONGO_IP + ":" + MONGO_PORT;
 	public  final static String MONGO_DB_NAME  = "livraria";
