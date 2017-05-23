915df9f4e25b8eea82dbdfe0cb20bcfd255a566c
Soter Padua <spadua@2mundos.net>
2017-05-23 07:41:39 -0300

    Trabalho inicial no Bean de Admin.

---
+++ src/livraria/beans/Admin.java
@@ -0,0 +1,26 @@
+package livraria.beans;
+
+/**
+ * Forged by Soter Padua on 23/05/17.
+ */
+public class Admin {
+	private String  email;
+	private String  senha;
+
+	// Getters and Setters
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
