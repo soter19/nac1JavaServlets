5af4564c900049a5924c4ef3f856678fa7348327
Soter Padua <spadua@2mundos.net>
2017-05-23 06:12:45 -0300

    Carrinho WIP

--- src/livraria/managedBeans/CarrinhoMB.java
+++ src/livraria/managedBeans/CarrinhoMB.java
@@ -1,12 +1,16 @@
 package livraria.managedBeans;
 
+import livraria.beans.Livro;
+
 import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
 
 /**
  * Forged by Soter Padua on 23/05/17.
  */
 @ManagedBean
 public class CarrinhoMB {
+	static ArrayList<Livro> carrinho;
 
 	public Boolean adicionaCarrinho(){
 		return false;
