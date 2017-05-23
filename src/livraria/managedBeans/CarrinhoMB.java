4360952766f9281d6048cb6beed242fc2bae8427
Soter Padua <spadua@2mundos.net>
2017-05-23 07:33:31 -0300

    Implementação básica do carrinho.

--- src/livraria/managedBeans/CarrinhoMB.java
+++ src/livraria/managedBeans/CarrinhoMB.java
@@ -3,20 +3,30 @@ package livraria.managedBeans;
 import livraria.beans.Livro;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
 import java.util.ArrayList;
 
 /**
  * Forged by Soter Padua on 23/05/17.
  */
 @ManagedBean
+@SessionScoped
 public class CarrinhoMB {
-	static ArrayList<Livro> carrinho;
+	private ArrayList<Livro> carrinho = new ArrayList<>();
 
-	public Boolean adicionaCarrinho(){
-		return false;
+	public ArrayList<Livro> getCarrinho() {
+		return carrinho;
 	}
 
-	public Boolean adicionaFavorito(){
-		return false;
+	public void adicionaAoCarrinho(Livro l){
+		carrinho.add(l);
+	}
+
+	public double valorTotal(){
+		double total = 0;
+		for(Livro l : carrinho){
+			total += l.getValor();
+		}
+		return total;
 	}
 }
